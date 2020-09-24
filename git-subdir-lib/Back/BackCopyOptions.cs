837b95a78dd230ea3325b61a588e03f09cc4abca
anatawa12 <anatawa12@icloud.com>
2020-09-24 17:33:07 +0900

    fix: DirInSrcs was readonly

--- git-subdir-lib/Back/BackCopyOptions.cs
+++ git-subdir-lib/Back/BackCopyOptions.cs
@@ -15,7 +15,7 @@ namespace GitSubdirTools.Libs.Back
         public string        SubdirDesc    { get; }
         public ObjectIdCache ObjectMapping { get; init; } = new ObjectIdCache(null);
         public Logger?       Logger        { get; init; } = null;
-        public IList<string> DirInSrcs     { get; }       = Array.Empty<string>();
+        public IList<string> DirInSrcs     { get; init; } = Array.Empty<string>();
         public IList<Branch> Branches      { get; init; } = new Branch[0];
         public int           MaxDepth      { get; init; } = -1;
     }
